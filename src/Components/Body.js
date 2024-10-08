4c66802a19cd77b9fa6187d1971ebbba861e45b5
utkarsh <utkarsh62@deloitte.com>
2024-10-08 17:48:52 +0530

    added images

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,165 +1,56 @@
 import React from 'react';
 import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import { useEffect } from "react";
+
 
 // Main body
 const Body = () =>  {
     // useState Hook
-        let [listOfRestaurants, setlistOfRestaurants] = useState([
-        {
-            data: {
-            id: "255655",
-            name: "Cake & Cream",
-            cloudinaryImageId: "ac57cc371e73f96f812613f58457aca3",
-            areaName: "Jairaj Nagar",
-            costForTwo: "₹200 for two",
-            cuisines: ["Bakery", "Hot-dog", "pastery", "Cake", "Thick- shake"],
-            avgRating: 4.3,
-            veg: true,
-            parentId: "54670",
-            avgRatingString: "4",
-            totalRatingsString: "20+",
-            },
-        },
-        {
-            data: {
-            id: "350363",
-            name: "Haldiram's Sweets and Namkeen",
-            cloudinaryImageId: "25c3a7d394d6c5556b134385f7d665b0",
-            avgRating: 4.6,
-            veg: true,
-            cuisines: [
-            "North Indian",
-            "South Indian",
-            "Chinese",
-            "Pizzas",
-            "Fast Food",
-            ],
-            parentId: "391465",
-            avgRatingString: "4.6",
-            totalRatingsString: "100+",
-            },
-        },
-        {
-            data: {
-            id: "154891",
-            name: "Rasraj Restaurant",
-            cloudinaryImageId: "egbr63ulc8h1zgliivd8",
-            locality: "Civil Line",
-            areaName: "Civil Lines",
-            costForTwo: "₹250 for two",
-            cuisines: [
-            "North Indian",
-            "South Indian",
-            "Street Food",
-            "Chinese",
-            "Pizzas",
-            "Fast Food",
-            ],
-            avgRating: 4.2,
-            },
-        },
-        {
-            data: {
-            id: "745961",
-            name: "Balaji Restaurant",
-            cloudinaryImageId: "b8672fe52944c3599ea324d99d608300",
-            locality: "Sai Rubber Stamp",
-            areaName: "Jairaj Nagar",
-            costForTwo: "₹149 for two",
-            cuisines: ["South Indian", "North Indian"],
-            avgRating: 4.8,
-            veg: true,
-            },
-        },
-        {
-            data: {
-            id: "798745",
-            name: "Friends Restaurant",
-            cloudinaryImageId: "b14cd9fc40129fcfb97aa7e621719d07",
-            locality: "Gayatri Nagar",
-            areaName: "Jairaj Nagar",
-            costForTwo: "₹150 for two",
-            cuisines: ["North Indian", "Chinese", "Biryani", "Tandoor", "Kebabs"],
-            avgRating: 4.2,
-            parentId: "84308",
-            },
-        },
-        {
-            data: {
-            id: "314737",
-            name: "RASOI the KITCHEN",
-            cloudinaryImageId: "yjymo9nhyn7rhvafsrd3",
-            locality: "Sriram Chowk",
-            areaName: "Bazar Ward",
-            costForTwo: "₹200 for two",
-            cuisines: ["North Indian", "Maharashtrian", "Chinese","Thalis"],
-            avgRating: 3.9,
-            parentId: "167341",
-            },
-        },
-        {
-            data: {
-            id: "201454",
-            name: "Morsels restaurants",
-            cloudinaryImageId: "aafe71251ef5328784652dc838cd91f3",
-            locality: "Bazar Ward",
-            areaName: "Chandrapur Locality",
-            costForTwo: "₹300 for two",
-            cuisines: ["North Indian", "South Indian"],
-            avgRating: 3.2,
-            veg: true,
-            parentId: "139266",
-            avgRatingString: "4.2",
-            },
-        },
-        {
-            data: {
-            id: "266124",
-            name: "Trimurti Restaurant",
-            cloudinaryImageId: "8135c0066b06e2925c66930be4e9ffb5",
-            locality: "Bazar Ward",
-            areaName: "Chandrapur Locality",
-            costForTwo: "₹150 for two",
-            cuisines: ["Desserts"],
-            avgRating: 3.9,
-            veg: true,
-            parentId: "217751",
-            avgRatingString: "4.4",
-            },
-        },
-        {
-            data: {
-            id: "509254",
-            name: "Saha Restaurant",
-            cloudinaryImageId: "z1ez4uc9idul2uj2v87g",
-            areaName: "Jairaj Nagar",
-            costForTwo: "₹300 for two",
-            cuisines: ["North Indian", "Biryani", "Thalis", "Beverages"],
-            avgRating: 3.7,
-            parentId: "174585",
-            avgRatingString: "3.7",
-            },
-        }
-    ]);
+        let [listOfRestaurants, setlistOfRestaurants] = useState([]);
+        let [listOfFilteredRestaurants, setlistOfFilteredRestaurants] = useState([]);
+        const [searchText, setsearchText] = useState("");
+
+
+    // hook which gets invoked after the component is rendered.
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.2961468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        console.log(json);
+        setlistOfRestaurants(json.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setlistOfFilteredRestaurants(json.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
 
     return (
         <div className="body">
-        <div className="search-bar">Search</div>
-        <div className="filter">
-            <button className="filter-btn" onClick={() => {
-                const filteredList = listOfRestaurants.filter((restaurant) => {return restaurant.data.avgRating > 4});
-                setlistOfRestaurants(filteredList);
-            }}>
-                Filter Top Rated
-            </button>
+            <div className="search">
+            <input type="text" className="search-box" value={searchText} onchange={(e)=>{setsearchText(e.target.value)}}/>
+            <button className="searchBtn" onClick={() => {
+                console.log(searchText);
+            }}>Search</button>
+            </div>
+
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const filteredList = listOfRestaurants.filter((restaurant) => {return restaurant.data.avgRating > 4});
+                    setlistOfFilteredRestaurants(filteredList);
+                }}>
+                    Filter Top Rated
+                </button>
+            </div>
+
+            <div className="res-container">
+            {listOfRestaurants.map((restaurant) => (
+                <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+            ))}  
+            </div>
         </div>
-        <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
-        ))}  
-        </div>
-    </div>
     );
 }
 
--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,10 +1,15 @@
+import { CDN_URL } from '../Utils/constants';
+
 const RestaurantCard = (props) => {
     const {resData} = props;
+    const {name,areaName,avgRating} = resData?.info;
+    const cdn_url = CDN_URL + resData.info?.cloudinaryImageId;
     return (
         <div className="res-card"> 
-            <h4>{resData.data.name}</h4>
-            <h4>{resData.data.areaName}</h4>
-            <h4>{resData.data.avgRating}</h4>
+            <img className="rounded-lg" alt="res-logo" src={cdn_url}/>
+            <h4>{name}</h4>
+            <h4>{areaName}</h4>
+            <h4>{avgRating}</h4>
         </div>
     );
 }
